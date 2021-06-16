a9ba798ad9952f691aa79b64efef9f0ea814df44
vv <victor.o.tomanik@gmail.com>
2021-06-16 18:06:56 -0300

    Adicionando download file

--- server/blueprints/views.py
+++ server/blueprints/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect,send_file
 
+import os
 
 def init_app(app):
 
@@ -11,12 +12,17 @@ def init_app(app):
     def eyestorm():
         return render_template('eyestorm.html')
 
-    @app.route("/upload-file", methods=['GET','POST'])
+    @app.route("/upload-file", methods=['POST'])
     def upload_file():
         #Verifica o nome do arquivo que foi recebido
         if request.files['dataset']:
             dataset = request.files['dataset']
-            print(dataset)
-            
+
+            # TODO: Ver o tamanho do arquivo (cookie) e se a extensao esta ok
+            dataset.save(os.path.join(app.config['data_folder'],dataset.filename))
         # Redireciona para a url que recebeu o post
         return redirect(request.referrer) 
+
+    @app.route("/download-file")
+    def download_file(name):
+        send_file(os.path.join(app.config['data_folder'],name),as_attachment=True)
