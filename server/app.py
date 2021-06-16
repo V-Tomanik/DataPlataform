0b8d20de004e9d3478940deae50aa77f2294706d
vv <victor.o.tomanik@gmail.com>
2021-06-15 23:57:00 -0300

    Adicionando adicao de arquivos

--- server/app.py
+++ server/app.py
@@ -4,8 +4,8 @@ from server.ext import configuration
 
 #Flask reconhece essa nomenclatura para inicialização do app
 def create_app():
-    app = Flask(__name__)
-    configuration.config_server(app)
+    app = Flask(__name__,template_folder='./blueprints/templates')
+    configuration.config_server(app,settings_files=["settings.toml"])
     configuration.load_extensions(app)
     return app
 
--- server/blueprints/views.py
+++ server/blueprints/views.py
@@ -1,12 +1,22 @@
-#from flask import render_template, abort
+from flask import render_template, request, redirect
 
 
 def init_app(app):
 
     @app.route("/")
     def index():
-        return "teste"
+        return "HomePage Flask"
 
     @app.route("/eyestorm")
     def eyestorm():
-        ...
+        return render_template('eyestorm.html')
+
+    @app.route("/upload-file", methods=['GET','POST'])
+    def upload_file():
+        #Verifica o nome do arquivo que foi recebido
+        if request.files['dataset']:
+            dataset = request.files['dataset']
+            print(dataset)
+            
+        # Redireciona para a url que recebeu o post
+        return redirect(request.referrer) 
