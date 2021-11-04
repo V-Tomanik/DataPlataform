b878bcc3b4fb8fae077d8bf14b0b83d0cc47db07
vv <victor.o.tomanik@gmail.com>
2021-11-03 22:18:11 -0300

    Adicionando um calendar descente

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # localserver
 
-Servidor Flask hospedado no meu raspberry pi 
-para uso pessoal e familiar, tendo como principio objetivo hospedar outros projetos
-
-ROADMAP:
-  - Visualizador de big data [ONGOING]
-  - Processo para armazenar arquivos no Server [PLANNED]
-  - Visualizador de Ações & Performance de Carteira de Investimento [PLANNED] 
+Projeto de estudo para desenvolver uma plataforma de ingestão de dados
+hospedado no Heroku e desenvolvido em Flask
--- server/blueprints/site/main.py
+++ server/blueprints/site/main.py
@@ -6,28 +6,36 @@ from flask import Blueprint
 pb = Blueprint("MainSite",__name__,template_folder="templates",static_folder="statics")
 
 
-@pb.route("/")
-def index():
-        """ Página Inicial do Site """
-        return "HomePage Flask"
+
+events=[
+        {'nome':'Entrega PI Novembro','date':'2021-11-03','color':'#E83F5B'},
+        {'nome':'Entrega MT Novembro','date':'2021-11-07','color':'#3485FF'},
+        {'nome':'Entrega Programming Index Novembro','date':'2021-11-23','color':'green'},
+
+        {'nome':'Entrega PI Outubro','date':'2021-10-03','color':'#E83F5B'},
+        {'nome':'Entrega MT Outubro','date':'2021-10-07','color':'#3485FF'},
+        {'nome':'Entrega Programming Index Outubro','date':'2021-10-23','color':'green'},
+]
+
 
 @pb.route("/eyestorm")
 def eyestorm():
     """ Página para tratamento e visualização de dados"""
     return render_template('eyestorm.html')
 
-@pb.route("/central")
+@pb.route("/")
+@pb.route("/entregas")
 def import_files_central():
-    """Central do site """
-    return render_template('central.html')
-
-@pb.route("/central/calendario")
-def calendario_central():
-    """Central do site """
-    return render_template('central_calendario.html')
-
-@pb.route("/central/entregas")
-def entregas_central():
-    """Central do site """
+    """Rota para entrega de arquivos """
     return render_template('central_entregas.html')
+
+@pb.route("/calendario")
+def calendario_central():
+    """Rota para visualizar o calendário de entregas """
+    return render_template('central_calendario.html',eventos=events)
+
+@pb.route("/summary")
+def entregas_central():
+    """Rota para visualizar o resumo de pendencias"""
+    return render_template('central_summary.html')
 
