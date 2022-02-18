9d528e0712faeed95d504dd45d759dad1b1020f3
vv <victor.o.tomanik@gmail.com>
2022-02-18 15:10:48 -0300

    Importando flex-layout

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 const MaterialComponents = [MatSidenavModule,
 														MatToolbarModule,
 														MatIconModule,
-														MatButtonModule];
+														MatButtonModule,
+														FlexLayoutModule];
 
 
 @NgModule({
